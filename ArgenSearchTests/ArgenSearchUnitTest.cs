3880038afef1a39f085e945d703702084f2a3214
Matias Dieguez <matiasdieguez@hotmail.com>
2017-08-19 23:26:04 -0300

    commit core y test

---
+++ ArgenSearchTests/ArgenSearchUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using ArgenSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ArgenSearchTests
+{
+    [TestClass]
+    public class ArgenSearchUnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var persona = ArgenSearchClient.Search(23456789).Result;
+            var id = persona.Data[0];
+
+            var datos = ArgenSearchClient.Detail(id).Result;
+        }
+    }
+}
---
+++ ArgenSearchTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArgenSearchTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArgenSearchTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11be458f-f152-45f9-8d50-f2f65739bd4c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
